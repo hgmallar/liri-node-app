68f0785bae1f953b2d7843fa47798539ee9696ce
Hilary Mallar <hilarymallar@gmail.com>
2018-11-08 16:00:06 -0500

    finished

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # liri-node-app
-liri node app
+## liri node app
+
+### Preview Demo at:
+[Video Demonstration Here](https://drive.google.com/file/d/18jORbpAmDrdgnWcPZ8Nb2pMgU_jcVw1n/view)
+
+### Possible Inputs: 
+* concert-this Artist's Name
+* spotify-this-song Song Title 
+* movie-this Movie Title
+* do-what-it says
+
+#### concert-this:
+* Example: **concert-this**
+    * returns that you must enter an artist
+* Example: **concert-this Taylor Swift**
+    * returns the venue, location, and dates of all of the artists upcoming concerts
+    
+#### spotify-this-song:
+* Example: **spotify-this-song**
+    * returns the, song name, and album of Ace of Base's The Sign, as well as a preview
+
+* Example: **spotify-this-song Shape Of You**
+    * returns the artist, song name, and album of the song entered, as well as a preview (if one exists)
+
+#### movie-this:  
+* Example: **movie-this**
+    * returns the movie name, releases date, IMDB rating, Rotten Tomatoes rating, plot, the actors, production company and language for the movie Mr. Nobody.
+* Example: **movie-this Gleaming the Cube**
+    * returns the movie name, releases date, IMDB rating, Rotten Tomatoes rating, plot, the actors, production company and language for the given movie title.
+
+#### do-what-it-says:
+* Example: **do-what-it-says**
+    * parses the information in random.txt and performs the commands on each line
+
+### Results: 
+* Requires **dot-env**, **node-spotify-api**, **request**, and **moment** packages.
+* All inputs and outputs get logged into a file name **log.txt**.
+* Uses **Bands In Town API** to get the data for concert-this.
+* Uses **Spotify API** to get the data for spotify-this-song.  
+* Uses **OMDB API** to get the data for movie-this.  
+
--- liri.js
+++ liri.js
@@ -9,7 +9,9 @@ const keys = require('./keys');
 
 var spotify = new Spotify(keys.spotify);
 
+//function to perform correct output for concert-this command
 function concertThis(artist) {
+    //create the url using the artist
     var url = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=" + keys.bandsInTown.id;
     request(url, function (error, response, body) {
         // If the request is successful (i.e. if the response status code is 200)
@@ -18,8 +20,10 @@ function concertThis(artist) {
             var json = JSON.parse(body); // `json` will be an array of objects
             console.log();
             for (i = 0; i < json.length; i++) {
+                //print a line for each item in the array
                 data = json[i];
                 var date = data.datetime.substr(0, data.datetime.indexOf('T'));
+                //convert the date to the correct format
                 date = moment(date, "YYYY-MM-DD").format('L');
                 var outputString = data.venue.name + " in " + data.venue.city + ", " + data.venue.country + " on " + date;
                 console.log(outputString);
@@ -27,44 +31,58 @@ function concertThis(artist) {
             }
         }
         else if (error) {
+            //log the error
             console.error('Error occurred: ' + error);
+            writeFile('Error occurred: ' + error + "\n");
         }
     });
 };
 
+//function to perform correct output for spotify-this-song command
 function spotifyThisSong(songName) {
+    //if there was no track inputed, set the track name to The Sign by Ace of Base
     if (songName === 'track:""') {
         songName = 'track:"The Sign" artist:"Ace of Base"';
     }
+    //spotify search using the songname
     spotify.search({ type: 'track', query: songName, limit: 1 }, function (err, data) {
         if (err) {
+            //log the error
             console.error('Error occurred: ' + err);
+            writeFile('Error occurred: ' + err + "\n");
         }
         var sing = " sings ";
+        //if the group is plural, correct the verb
         if (data.tracks.items[0].album.artists[0].name.charAt(data.tracks.items[0].album.artists[0].name.length - 1) === "s") {
             sing = " sing ";
         }
+        //create an output string with the returned data
         var outputString = data.tracks.items[0].album.artists[0].name + sing + data.tracks.items[0].name + " on the album " + data.tracks.items[0].album.name;
         console.log();
         console.log(outputString);
-        writeFile(outputString + "\n");
+        writeFile(outputString + " \n");
+        //check to see if there is in fact a preview of the track before printing the message
         if (data.tracks.items[0].preview_url != null) {
             outputString = "A preview can be found at " + data.tracks.items[0].preview_url;
             console.log(outputString);
-            writeFile(outputString + "\n");
+            writeFile(outputString + " \n");
         }
     });
 };
 
+//function to perform correct output for movie-this command
 function movieThis(movieName) {
+    //if there isn't a movie name, use Mr. Nobody
     if (movieName === "") {
         movieName = "Mr. Nobody";
     }
     var request = require("request");
+    //create a queryUrl using the moviename
     var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+    //make an OMDB API request
     request(queryUrl, function (error, response, body) {
         if (!error && response.statusCode === 200) {
-
+            //print the information returned
             var json = JSON.parse(body); // `json` will be an array of objects
             console.log();
             var outputString = json.Title + " was released in " + json.Year + "."
@@ -84,86 +102,115 @@ function movieThis(movieName) {
             writeFile(outputString + "\n");
         }
         else if (error) {
+            //log the error
             console.error('Error occurred: ' + error);
+            writeFile('Error occurred: ' + error + "\n");
         }
     });
 }
 
+//function to append text to the log.txt file
 function writeFile(outputText) {
+    //append the text passed in to the log.txt file
     fs.appendFile("log.txt", outputText, function (err) {
-        // If the code experiences any errors it will log the error to the console.
+        // if the code experiences any errors it will log the error to the console
         if (err) {
             return console.log(err);
         }
     });
 };
 
+//if the input is concert-this
 if (process.argv[2] === "concert-this") {
     // Get all elements in process.argv, starting from index 3 to the end
     // Join them into a string to get the space delimited artist
     var artist = process.argv.slice(3).join(" ");
+    //write the command to the log.txt file
+    writeFile(" \n");
     if (artist) {
-        writeFile("\n");
+        //print the required info
         writeFile(process.argv.slice(2).join(" ").trim() + "\n");
         concertThis(artist);
     }
     else {
+        //you must enter an artist
+        writeFile(process.argv[2]);
         console.log("You must enter an artist!");
         writeFile("You must enter an artist!" + "\n");
     }
 }
+//if the input is spotify-this-song
 else if (process.argv[2] === "spotify-this-song") {
     // Get all elements in process.argv, starting from index 3 to the end
     // Join them into a string to get the space delimited songName
     var songName = "";
     songName = 'track:"' + process.argv.slice(3).join("+") + '"';
-    writeFile("\n");
+    //write the command to the log.txt file
+    writeFile(" \n");
     writeFile(process.argv.slice(2).join(" ") + "\n");
+    //print the required info
     spotifyThisSong(songName);
 }
+//if the input is movie-this
 else if (process.argv[2] === "movie-this") {
     // Get all elements in process.argv, starting from index 3 to the end
     // Join them into a string to get the space delimited movieName
     var movieName = "";
     movieName = process.argv.slice(3).join("+");
-    writeFile("\n");
+    //write the command to the log.txt file
+    writeFile(" \n");
     writeFile(process.argv.slice(2).join(" ") + "\n");
+    //print the required info
     movieThis(movieName);
 }
+//if the input is do-what-it-says
 else if (process.argv[2] === "do-what-it-says") {
-    writeFile("\n");
+    //write the command to the log.txt file
+    writeFile(" \n");
     writeFile(process.argv.slice(2).join(" ") + "\n");
     fs.readFile("random.txt", "utf8", function (error, data) {
 
-        // If the code experiences any errors, log the error to the console.
+        // if the code experiences any errors, log the error
         if (error) {
             return console.log(error);
+            writeFile(error + "\n");
         }
 
-        writeFile(data + "\n");
-
-        // Then split it by commas (to make it more readable)
-        var dataArr = data.split(",");
-
-        if (dataArr[0] === "concert-this") {
-            concertThis(dataArr[1]);
-        }
-        else if (dataArr[0] === "spotify-this-song") {
-            var songName = "";
-            songName = 'track:' + dataArr[1];
-            spotifyThisSong(songName);
-        }
-        else if (dataArr[0] === "movie-this") {
-            movieThis(dataArr[1].replace(/ /g, '+'));
-        }
-        else {
-            console.log(dataArr[0] + " is not a recognized command!");
-            writeFile(dataArr[0] + " is not a recognized command!\n");
+        //split the command in random.txt by lines
+        var dataArr = data.split("\n")
+        for (var i = 0; i < dataArr.length; i++) {
+            var dataArrLine = dataArr[i].split(",")
+            //if the first item is concert-this
+            if (dataArrLine[0] === "concert-this") {
+                //get the info from the API and print the necessary info using the second item in the array as artist
+                concertThis(dataArrLine[1]);
+            }
+            //if the first item is spotify-this-song
+            else if (dataArrLine[0] === "spotify-this-song") {
+                //use the second item in the array as songName
+                var songName = "";
+                songName = 'track:' + dataArrLine[1];
+                //get the info from the API and print the necessary info
+                spotifyThisSong(songName);
+            }
+            //if the first item is movie-this
+            else if (dataArrLine[0] === "movie-this") {
+                //get the info from the API and print the necessary info using the second item in the array as the movieName
+                movieThis(dataArrLine[1].replace(/ /g, '+'));
+            }
+            //there isn't a correct command written in random.txt
+            else {
+                //log the error
+                console.log(dataArrLine[0] + " is not a recognized command!");
+                writeFile(dataArrLine[0] + " is not a recognized command!\n");
+            }
         }
 
     });
 }
+//either there wasn't a command, or there wasn't a recognized command
 else {
+    //log the error
     console.log(process.argv[2] + " is not a recognized command!");
     writeFile(process.argv[2] + " is not a recognized command!\n")
 }
